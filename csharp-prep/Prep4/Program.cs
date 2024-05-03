15d3f585064d61d00fbe238c4d0a7bba7afa5fcc
Joshua Macias <mac22045@byui.edu>
2024-05-03 14:50:30 -0600

    Assignment Friday

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        var numbers = new List<int>();
+        
+        int userNumber;
+        do
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            userNumber = int.Parse(Console.ReadLine());
+            
+            if (userNumber != 0)
+                numbers.Add(userNumber);
+            
+        } while (userNumber != 0);
+
+        // Part 1: Compute the sum
+        int sum = 0;
+        foreach (var number in numbers)
+            sum += number;
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Part 2: Compute the average
+        float average = sum / (float)numbers.Count;
+        string average_formatted = average.ToString("F2");
+        Console.WriteLine($"The average is: {average_formatted}");
+
+        // Part 3: Find the max
+        int max = int.MinValue;
+        foreach (var number in numbers)
+        {
+            if (number > max)
+                max = number;
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMessage();
+
+        string userName = GetUserName();
+        int userNumber = GetUserNumber();
+
+        int squaredNumber = CalculateSquare(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+
+    static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int CalculateSquare(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, your number at the 2 power is {square}");
     }
 }
